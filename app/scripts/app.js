90675e27f166cff0d7b8ddbc56338cece9fec7ac
lik <961471331@qq.com>
2016-12-19 15:17:18 +0800

    初始化
    Signed-off-by: lik <961471331@qq.com>

---
+++ app/scripts/app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name yeomanApp
+ * @description
+ * # yeomanApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('yeomanApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ]);
