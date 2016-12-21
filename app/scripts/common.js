263822d5859d17cdc37e480e201d6ab818b0118e
yy <1291551591@qq.com>
2016-12-21 08:18:10 +0800

    pengyan 第一天

---
+++ app/scripts/common.js
@@ -0,0 +1,6 @@
+$(function() {
+document.documentElement.style.fontSize = document.documentElement.clientWidth / 16 + 'px';
+window.onresize = function() {
+document.documentElement.style.fontSize = document.documentElement.clientWidth / 16 + 'px';
+}
+}) 
---
+++ app/scripts/yy.js
@@ -0,0 +1,13 @@
+angular.module('yeomanApp',[]).directive("py", function() {
+		return {
+			restrict: "ECMA",
+			templateUrl: 'views/login.html',
+			scope:true,
+			replace:true,
+			link:function(s,e,a){
+				
+			}
+		}
+	})
+
+
