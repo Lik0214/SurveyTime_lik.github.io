9e2803d9db48ca6eafe310a680710aba95a510a3
yy <1291551591@qq.com>
2016-12-22 15:47:23 +0800

    peng

--- app/scripts/yy.js
+++ app/scripts/yy.js
@@ -26,23 +26,23 @@ angular.module('py', []).controller("pyan",["$scope","$http",function($scope,$ht
 			//	};
 			//产生验证码 
 			function createCode() {
-				code = "";
+				code1 = "";
 				var codeLength = 4; //验证码的长度  
 				//	     var checkCode = document.getElementById("code");   
 				var random = new Array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
 					'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'); //随机数  
 				for(var i = 0; i < codeLength; i++) { //循环操作  
 					var index = Math.floor(Math.random() * 36); //取得随机数的索引（0~35）  
-					code += random[index]; //根据索引取得随机数加到code上  
+					code1 += random[index]; //根据索引取得随机数加到code上  
 				}
-				$('#code').html(code); //把code值赋给验证码  
+				$('#code').html(code1); //把code值赋给验证码  
 			}
 			//校验验证码  
 			function validate() {
 				var inputCode = document.getElementById("input").value.toUpperCase(); //取得输入的验证码并转化为大写        
 				if(inputCode.length <= 0) { //若输入的验证码长度为0  
 					alert("请输入验证码！"); //则弹出请输入验证码  
-				} else if(inputCode != code) { //若输入的验证码与产生的验证码不一致时  
+				} else if(inputCode != code1) { //若输入的验证码与产生的验证码不一致时  
 					alert("验证码输入错误！");
 					document.getElementById("input").value = "";
 					//清空文本框  
@@ -86,11 +86,11 @@ angular.module('py', []).controller("pyan",["$scope","$http",function($scope,$ht
 			}).then(function(e){
 				console.log(e)
 			},function(){
-				alert('用户名已存在');
-//				$scope.borrow="用户名已存在!";
-//				$scope.close = function(){
-//					$scope.y_background = 'y_background';
-//				}
+				$scope.y_background = 'y_background1';
+				$scope.close = function(){
+					$scope.y_background = 'y_background';
+				}
+				$scope.borrow="用户名已存在!";
 			});
 			
 		}
@@ -130,11 +130,11 @@ angular.module('py', []).controller("pyan",["$scope","$http",function($scope,$ht
 				}).then(function(e){
 					console.log(e)
 				},function(){
-					alert('用户名或密码不正确!')
-//					$scope.borrow="用户名或密码不正确!";
-//					$scope.close = function(){
-//						$scope.y_background = 'y_background';
-//					}
+					$scope.y_background = 'y_background1';
+					$scope.borrow="用户名密码不一致 !";
+					$scope.close = function(){
+						$scope.y_background = 'y_background';
+					}
 				});
 //			console.log($scope.updata)
 //			$http({
