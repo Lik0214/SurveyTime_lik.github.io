48bdb465a8f3e177e5621867b87358283c8e67c0
songyanjun123 <727415690@qq.com>
2016-12-24 15:31:44 +0800

    updater syjstics.js  12/24 Signed-off-by: songyanjun123 <727415690@qq.com>

--- app/scripts/syjstics.js
+++ app/scripts/syjstics.js
@@ -6,10 +6,32 @@ angular.module('syj',["chart.js"]).controller("syjCtrl", ["$scope","$http","getD
   $scope.dataa = [300, 500, 100];*/
   //$scope.server="http://47.90.20.200:1602/item";
   $scope.coo = window.localStorage.uid;
-  getData.post("http://47.90.20.200:1602/item",{uid:$scope.coo},function(respose){
+  getData.get("http://47.90.20.200:1602/item?uid="+$scope.coo,function(respose){
   	$scope.fruit=respose.data;
-  	console.log($scope.fruit)
+	/*console.log($scope.fruit);
+	console.log($scope.fruit[0]);
+	console.log($scope.fruit[0].option);
+	console.log($scope.fruit[0].option[0].opt);
+	console.log($scope.fruit[0].option[0].opt[0]);
+	console.log($scope.fruit[0].option[0].opt[0].op);*/
+	for(i in $scope.fruit){
+		// console.log($scope.fruit[i])
+		for(j in $scope.fruit[i].option){
+			//console.log($scope.fruit[i].option[j])
+			for(k in $scope.fruit[i].option[j].opt){
+				//console.log($scope.fruit[i].option[j].opt[k])
+			}
+		}
+	}
+	/*angular.forEach($scope.fruit,function(data,index,array){
+		console.log(data.a+'='+array[index].a);
+	})*/
+	/*scope.asd = ["body", "girl", "不男不女"];
+	$scope.datas = [300, 500, 100];
+	$scope.qwe = ["body", "girl", "不男不女"];
+	$scope.dataa = [300, 500, 100];*/
   })
+
 	/*$http({
   		url:$scope.server,
      	method:"get"
