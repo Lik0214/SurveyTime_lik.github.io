716e66c81c462bcd4213b86fef2af60136beb854
lik <961471331@qq.com>
2016-12-27 09:36:20 +0800

    lik  put  data
    Signed-off-by: lik <961471331@qq.com>

--- app/scripts/lik.js
+++ app/scripts/lik.js
@@ -5,35 +5,38 @@ angular.module('lik', [])
 		}
 		$scope.hash = window.location.hash
 		$scope.idNeed = $scope.hash.split('?')[1]
-//		console.log($scope.idNeed)
+			//		console.log($scope.idNeed)
 		$http({
 			method: 'get',
 			url: $rootScope.server + 'item?' + $scope.idNeed
 		}).then(function(e) {
 			$scope.likData = e.data
-			console.log($scope.likData.option)
 		}, function() {})
 		$scope.submit = function() {
-			console.log($scope.likData.option)
-			for(var i=0;i<$scope.likData.option.length;i++){
-				if($scope.likData.option[i].type==0){
-					$scope.likData.option[i].oop=$('.liktext').eq(i).val()
-				}else if($scope.likData.option[i].type==1){
-					/*for(var j=0;j<$scope.likData.option[i].opt.length;j++){
-						if($('.likradio')[j].attr('checked')=='true'){
-							$scope.likData.option[i].opt[j].num+=1
+			for(var i = 0; i < $scope.likData.option.length; i++) {
+				if($scope.likData.option[i].type == 0) {
+					$scope.likData.option[i].oop = $('.liktext').eq(i).val()
+				} else if($scope.likData.option[i].type == 1) {
+					for(var j = 0; j < $scope.likData.option[i].opt.length; j++) {
+						if($('.lik_survey_question').eq(i).find('input:radio').eq(j).prop('checked') == true) {
+							$scope.likData.option[i].opt[j].num +=1
 						}
-					}*/
-				}else if($scope.likData.option[i].type==3){
-					console.log(i)
-					$scope.likData.option[i].oop=$('.likarea').eq(i).val()
+					}
+				} else if($scope.likData.option[i].type == 2) {
+					for(var j = 0; j < $scope.likData.option[i].opt.length; j++) {
+						if($('.lik_survey_question').eq(i).find('input:checkbox').eq(j).prop('checked') == true) {
+							$scope.likData.option[i].opt[j].num +=1
+						}
+					}
+				} else if($scope.likData.option[i].type == 3) {
+					$scope.likData.option[i].oop = $('.likarea').eq(i).val()
 				}
 			}
-			console.log($scope.likData.option)
-			
+
 			$http({
 				url: $rootScope.server + 'item?' + $scope.idNeed,
-				method: 'put'
+				method: 'put',
+				data:$scope.likData
 			}).then(function() {
 
 			}, function() {})
