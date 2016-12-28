59806430b79bdfaa7a0a0b4eef2e0d6c2b6107c5
lik <961471331@qq.com>
2016-12-28 13:54:06 +0800

    lik error
    Signed-off-by: lik <961471331@qq.com>

--- app/scripts/gyr.js
+++ app/scripts/gyr.js
@@ -1,20 +1,16 @@
-angular.module('gyr', ['ui.router', 'angular-clipboard'])
+angular.module('gyr', ['ui.router'])
 	.directive('list2', function() {
 		return {
 			restrict: 'ECMA',
 			replace: true,
 			templateUrl: 'views/list.html'
 		}
-
 	}).controller('yr', ['$rootScope', '$scope', '$location', '$http', function($rootScope, $scope, $location, $http) {
 		$scope.go = function() {
 			$location.path('nav');
-<<<<<<< HEAD
 		}
 		$scope.goOn = function(a, b) {
 			$scope.gyr_luyou = 'http://www.surveytime.cn/1602/lik/dist/#!/share/servey?uid=' + a + '&id=' + b
-				//			gyr_xinas.style.display = 'block'
-=======
 			$http({
 				url: "http://47.90.20.200:1602/item?uid=" + window.localStorage.uid,
 				method: "get"
@@ -22,11 +18,6 @@ angular.module('gyr', ['ui.router', 'angular-clipboard'])
 				$scope.gyr_arr = e.data;
 				console.log(e);
 			}, function() {});
-		}
-		$scope.goOn = function(a,b) {
-			$scope.gyr_luyou = 'http://www.surveytime.cn/1602/lik/dist/#!/share/servey?uid='+a+'&id='+b
-//			gyr_xinas.style.display = 'block'
->>>>>>> origin/master
 		}
 		$scope.goSta = function(id) {
 			window.localStorage.id = $scope.gyr_arr[id].id;
@@ -53,11 +44,7 @@ angular.module('gyr', ['ui.router', 'angular-clipboard'])
 					method: 'delete'
 				}).then(function(e) {
 					$http({
-<<<<<<< HEAD
 						url: $rootScope.server + "item?uid=" + window.localStorage.uid,
-=======
-						url: "http://47.90.20.200:1602/item?uid=" + window.localStorage.uid,
->>>>>>> origin/master
 						method: "get"
 					}).then(function(e) {
 						$scope.gyr_arr = e.data;
--- app/scripts/lik.js
+++ app/scripts/lik.js
@@ -1,10 +1,6 @@
 angular.module('lik', [])
 	.controller('likCtrl', ['$rootScope', '$scope', '$location', '$http', function($rootScope, $scope, $location, $http) {
-		$scope.goBack = function() {
-			$location.path('nav')
-		}
-		$scope.hash = window.location.hash
-		$scope.idNeed = $scope.hash.split('?')[1]
+		$scope.idNeed = window.location.hash.split('?')[1]
 		$http({
 			method: 'get',
 			url: $rootScope.server + 'item?' + $scope.idNeed
@@ -12,39 +8,62 @@ angular.module('lik', [])
 			$scope.likData = e.data
 		}, function() {})
 		$scope.lik_success = false
-		$scope.submit = function() {
-			
-			for(var i = 0; i < $scope.likData.option.length; i++){
-				$scope.lik_text = $('.liktext').eq(i).val()
-				console.log($scope.lik_text)
-			}
-			
-			/*for(var i = 0; i < $scope.likData.option.length; i++) {
+		$scope.lik_submit = function() {
+			alert(1)
+			for(var i = 0; i < $scope.likData.option.length; i++) {
 				if($scope.likData.option[i].type == 0) {
-					$scope.likData.option[i].oop = $('.liktext').eq(i).val()
+					if($('.liktext').eq(i).val()==''){
+						$scope.lik_success = false
+						$scope.lik_success1 = 0
+					}else{
+						$scope.likData.option[i].oop = $('.liktext').eq(i).val()
+						$scope.lik_success1 = 1
+					}
 				} else if($scope.likData.option[i].type == 1) {
 					for(var j = 0; j < $scope.likData.option[i].opt.length; j++) {
-						if($('.lik_survey_question').eq(i).find('input:radio').eq(j).prop('checked') == true) {
+						if($('.lik_survey_question').eq(i).find('input:radio').eq(j).prop('checked') == false){
+							$scope.lik_success = false
+							$scope.lik_success2 = 0
+						}else{
 							$scope.likData.option[i].opt[j].num +=1
+							$scope.lik_success2 = 1
 						}
 					}
 				} else if($scope.likData.option[i].type == 2) {
 					for(var j = 0; j < $scope.likData.option[i].opt.length; j++) {
-						if($('.lik_survey_question').eq(i).find('input:checkbox').eq(j).prop('checked') == true) {
+						if($('.lik_survey_question').eq(i).find('input:checkbox').eq(j).prop('checked') == false){
+							$scope.lik_success = false
+							$scope.lik_success3 = 0
+						}else{
 							$scope.likData.option[i].opt[j].num +=1
+							$scope.lik_success3 = 1
 						}
 					}
 				} else if($scope.likData.option[i].type == 3) {
-					$scope.likData.option[i].oop = $('.likarea').eq(i).val()
+					if($('.likarea').eq(i).val()==''){
+						$scope.lik_success = false
+						$scope.lik_success4 = 0
+					}else{
+						$scope.likData.option[i].oop = $('.likarea').eq(i).val()
+						$scope.lik_success4 = 1
+					}
 				}
 			}
+			
+			if($scope.lik_success1 == 1 && $scope.lik_success2 == 1 && $scope.lik_success3 == 1 && $scope.lik_success4 == 1){
+				$scope.lik_success = true
+			}
 
-			$http({
-				url: $rootScope.server + 'item?' + $scope.idNeed,
-				method: 'put',
-				data:$scope.likData
-			}).then(function() {
-
-			}, function() {})*/
+			if($scope.lik_success){
+				$http({
+					url: $rootScope.server + 'item?' + $scope.idNeed,
+					method: 'put',
+					data:$scope.likData
+				}).then(function() {
+	
+				}, function() {})
+			}else{
+				alert('未完成调查问卷')
+			}
 		}
 	}])
