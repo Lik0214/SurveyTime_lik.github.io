1ec53298fbff74143355d4edef0b114a26da9921
lik <961471331@qq.com>
2016-12-29 19:52:15 +0800

    lik  summernote error Signed-off-by: lik <961471331@qq.com>

--- app/scripts/lik.js
+++ app/scripts/lik.js
@@ -1,5 +1,20 @@
-angular.module('lik', [])
+angular.module('lik', ['summernote'])
 	.controller('likCtrl', ['$rootScope', '$scope', '$location', '$http', function($rootScope, $scope, $location, $http) {
+		$scope.options = {
+			height: 300,
+			focus: true,
+			airMode: false,
+			toolbar: [
+				['headline', ['style']],
+				['style', ['bold', 'italic', 'underline', 'superscript', 'subscript', 'strikethrough', 'clear']],
+				['textsize', ['fontsize']],
+				['fontclr', ['color']],
+				['alignment', ['ul', 'ol', 'paragraph', 'lineheight']],
+				['height', ['height']],
+				['insert', ['picture', 'hr']],
+				['view', ['fullscreen']]
+			]
+		};
 		$scope.idNeed = window.location.hash.split('?')[1]
 		$http({
 			method: 'get',
@@ -15,7 +30,12 @@ angular.module('lik', [])
 		}, function() {})
 
 		$scope.lik_successs = ''
+			/*$scope.sumchange = function(contents) {
+				var reg = /<[^<]+>/g;
+				$scope.sumtext = contents.replace(reg,'')
+			};*/
 		$scope.lik_submit = function() {
+
 			$scope.lik_success = []
 				/*for(var i = 0; i < $scope.likData.option.length; i++) {
 					if($scope.likData.option[i].type == 0) {
@@ -71,8 +91,8 @@ angular.module('lik', [])
 						}
 					}
 				} else if($scope.likData.option[i].type == 3) {
-					if($('.likarea').eq(i).val() == '') {} else {
-						$scope.likData.option[i].oop = $('.likarea').eq(i).val()
+					if($('.likarea').eq(i).html() == '') {} else {
+						$scope.likData.option[i].oop = $('.likarea').eq(i).html()
 					}
 				}
 			}
@@ -87,10 +107,8 @@ angular.module('lik', [])
 					}
 					if($scope.str1 == $scope.str2) {
 						$scope.lik_success.push('false')
-
 					} else {
 						$scope.lik_success.push('true')
-
 					}
 				} else {
 					if($scope.likData.option[i].oop == $scope.lod_likai.option[i].oop) {
@@ -100,18 +118,28 @@ angular.module('lik', [])
 					}
 				}
 			}
-
-			if($scope.lik_success.lastIndexOf('false') != -1) {
+			if($scope.lik_success.indexOf('false') != -1) {
 				alert('请完善调查问卷')
 			} else {
 				$http({
 					url: $rootScope.server + 'item?' + $scope.idNeed,
 					method: 'put',
 					data: $scope.likData
-				}).then(function() {
-
-				}, function() {})
-				window.location.hash = '#!/thanks'
+				}).then(function() {}, function() {})
+//				window.location.hash = '#!/thanks'
 			}
 		}
 	}])
+	/*.directive('liksum', function(){
+		return {
+			restrict:"ECMA",
+			scope:{},
+			template:'<summernote ng-show="x.type == 3" config="options" on-change="sumchange(contents)" ng-model="text" ng-class="summernote"></summernote>',
+			link:function(s,e,a){
+				$('#submitBtn').click(function(){
+					console.log(s.text)
+				})
+			}
+		}
+	})
+*/
